0fc39cbcb893cdfc40c05ca9670228f9889c817c
Raj Advani <radvani@viromedia.com>
2018-07-12 23:45:43 -0400

    Update copyrights

--- CrowdControls/AppDelegate.m
+++ CrowdControls/AppDelegate.m
@@ -3,8 +3,7 @@
 //  CrowdControls
 //
 //  Created by Raj Advani on 5/13/18.
-//  Copyright (c) 2018 Raj Advani
-
+//
 //  Permission is hereby granted, free of charge, to any person obtaining a copy
 //  of this software and associated documentation files (the "Software"), to deal
 //  in the Software without restriction, including without limitation the rights
